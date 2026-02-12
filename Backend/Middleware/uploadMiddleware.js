ac0eefc6928d91f1becca4be63e177ef4f813c85
zougzed-blip <zougzed@gmail.com>
2026-02-12 17:00:58 +0200

    Update uploadMiddleware.js

--- Backend/Middleware/uploadMiddleware.js
+++ Backend/Middleware/uploadMiddleware.js
@@ -1,5 +1,5 @@
 const multer = require('multer');
-const cloudinary = require('../config/cloudinary');
+const cloudinary = require('../config/Cloudinary');
 
 const storage = multer.memoryStorage();
 
