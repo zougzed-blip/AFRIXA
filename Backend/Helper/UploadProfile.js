ef944560d38538dbd4ff53834b5373f50eb35b35
zougzed-blip <zougzed@gmail.com>
2026-02-12 15:28:22 +0200

    Update UploadProfile.js

--- Backend/Helper/UploadProfile.js
+++ Backend/Helper/UploadProfile.js
@@ -1,4 +1,4 @@
-const cloudinary = require('../config/cloudinary')
+const cloudinary = require('../config/Cloudinary')
 
 const uploadToCloudinary = (buffer, folder) => {
   return new Promise((resolve, reject) => {
